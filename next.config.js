d7b8a69e719694306e7a818d24898863edf2391a
Enrico <54645197+enricoschaaf@users.noreply.github.com>
2020-04-30 01:48:31 +0200

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  experimental: { reactRefresh: true },
+  reactStrictMode: true
+}
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+import { IndexComponent } from "../components"
+
+const Index = () => (
+  <>
+    <Head>
+      <title>Title</title>
+    </Head>
+    <IndexComponent />
+  </>
+)
+
+export default Index
