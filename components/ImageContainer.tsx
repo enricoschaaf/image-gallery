23b04aa67f574fbcfa2b95c5816d3f642ade2dd1
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-05-03 01:25:22 +0200

    finished

--- components/ImageContainer.tsx
+++ components/ImageContainer.tsx
@@ -8,19 +8,22 @@ export const ImageContainer = ({ data }) => {
     if (ref.current) setOffsetWidth(ref.current.offsetWidth)
   }, [])
 
-  const oneThirdLength = Math.floor(data.length / 3)
-  const remainder = data.length % 3
-  const firstCol = data.slice(0, oneThirdLength)
-  const secondCol = data.slice(oneThirdLength, 2 * oneThirdLength)
-  const thirdCol = data.slice(2 * oneThirdLength, data.length)
+  const test = data.flat()
+  const oneThirdLength = Math.floor(test.length / 3)
+  const firstCol = test.slice(0, oneThirdLength)
+  const secondCol = test.slice(oneThirdLength, 2 * oneThirdLength)
+  const thirdCol = test.slice(2 * oneThirdLength, test.length)
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 row-gap-4 sm:row-gap-4 col-gap-4 sm:col-gap-6 lg:col-gap-8">
-      <div className="space-y-4 sm:space-y-6 lg:space-y-8" ref={ref}>
+    <div className="grid grid-cols-1 sm:grid-cols-3 sm:row-gap-4 col-gap-4 sm:col-gap-6 lg:col-gap-8 sm:pb-6 lg:pb-8">
+      <div
+        className="space-y-4 sm:space-y-6 lg:space-y-8 mb-4 sm:mb-0"
+        ref={ref}
+      >
         {thirdCol.map(result => (
           <Image key={result.id} result={result} offsetWidth={offsetWidth} />
         ))}
       </div>
-      <div className="space-y-4 sm:space-y-6 lg:space-y-8">
+      <div className="space-y-4 sm:space-y-6 lg:space-y-8 mb-4 sm:mb-0">
         {secondCol.map(result => (
           <Image key={result.id} result={result} offsetWidth={offsetWidth} />
         ))}
--- components/Page.tsx
+++ components/Page.tsx
@@ -1,9 +1,13 @@
 import Head from "next/head"
 import { useRouter } from "next/router"
 import { MutableRefObject, useEffect, useRef, useState } from "react"
+import { useInView } from "react-intersection-observer"
+import { useInfiniteQuery } from "react-query"
 import { ImageContainer, Search } from "."
-import { useFetch, useInfiniteScroll } from "../hooks"
+//@ts-ignore
 import BlankCanvas from "../public/blank-canvas.svg"
+//@ts-ignore
+import ServerDown from "../public/server-down.svg"
 
 function searchKeyboardShortcut(
   searchRef: MutableRefObject<HTMLInputElement>,
@@ -15,72 +19,130 @@ function searchKeyboardShortcut(
   }
 }
 
+const fetchImages = async (input, page = 1) => {
+  if (input === " ") {
+    const res = await fetch(
+      `https://api.unsplash.com/photos/?page=${page}&client_id=jcMNsi9XLMv8SE8mcoYq2cXu0JfuUrFh01IbonJSvAU`
+    )
+    return await res.json()
+  }
+  const res = await fetch(
+    `https://api.unsplash.com/search/photos?page=${page}&query=${input}&client_id=jcMNsi9XLMv8SE8mcoYq2cXu0JfuUrFh01IbonJSvAU`
+  )
+  const { results } = await res.json()
+  return results
+}
+
+const NoData = ({ setInput, searchRef }) => (
+  <>
+    <Head>
+      <title>Image Gallery</title>
+    </Head>
+    <div className=" px-4 sm:px-0 pb-4 sm:pb-6 lg:pb-8">
+      <Search searchRef={searchRef} setInput={setInput} />
+    </div>
+    <div className="flex flex-col flex-1 items-center space-y-4 sm:space-y-6 lg:space-y-8 justify-center">
+      <BlankCanvas className="w-96 px-4 sm:px-0" />
+      <span className="text-gray-600">
+        Sorry, we couldn't find any matches.
+      </span>
+    </div>
+  </>
+)
+
+const Error = ({ searchRef, setInput }) => (
+  <>
+    <Head>
+      <title>Image Gallery</title>
+    </Head>
+    <div className=" px-4 sm:px-0 pb-4 sm:pb-6 lg:pb-8">
+      <Search searchRef={searchRef} setInput={setInput} />
+    </div>
+    <div className="flex flex-col flex-1 items-center space-y-4 sm:space-y-6 lg:space-y-8 justify-center">
+      <ServerDown className="w-96 px-4 sm:px-0" />
+      <span className="text-gray-600">Oops! Something went wrong.</span>
+    </div>
+  </>
+)
+
+const Loading = ({ input, setInput, searchRef }) => (
+  <>
+    <Head>
+      <title>
+        {input !== ""
+          ? `${input.charAt(0).toUpperCase()}${input.slice(1).toLowerCase()} - `
+          : ""}
+        Image Gallery
+      </title>
+    </Head>
+    <div className=" px-4 sm:px-0 pb-4 sm:pb-6 lg:pb-8">
+      <Search searchRef={searchRef} setInput={setInput} />
+    </div>
+  </>
+)
+
 export const Page = () => {
   const router = useRouter()
-  const [input, setInput] = useState("")
-  const [count, setCount] = useState(1)
+  const [input, setInput] = useState(null)
   const searchRef = useRef(null)
+
+  const {
+    status,
+    data,
+    fetchMore,
+    //@ts-ignore
+  } = useInfiniteQuery(input, fetchImages, {
+    getFetchMore: (lastGroup, allGroups) => {
+      if (lastGroup.length === 10) return allGroups.length + 1
+    },
+  })
+
   useEffect(() => {
     if (typeof router.query.search === "string") setInput(router.query.search)
+    if (router.query.search === undefined) setInput(" ")
     window.addEventListener("keydown", e =>
       searchKeyboardShortcut(searchRef, e)
     )
     return () => {
-      removeEventListener("keydown", e => searchKeyboardShortcut(searchRef, e))
+      removeEventListener("keydown", () => searchKeyboardShortcut)
     }
   }, [router.query.search])
-  const { data } = useFetch(
-    input === ""
-      ? `https://api.unsplash.com/photos/?page=1&client_id=1aPC6HMbXRHmh4fY6QzSpY7OKhWf3x1zmx9NnNf8qKw`
-      : `https://api.unsplash.com/search/photos?page=${count}&query=${input}&client_id=1aPC6HMbXRHmh4fY6QzSpY7OKhWf3x1zmx9NnNf8qKw`
-  )
-  const { ref } = useInfiniteScroll(setCount)
-  return (
-    <>
-      <Head>
-        <title>
-          {input !== ""
-            ? `${input.charAt(0).toUpperCase()}${input
-                .slice(1)
-                .toLowerCase()} - `
-            : ""}
-          Image Gallery
-        </title>
-      </Head>
-      <div className="flex flex-col max-w-7xl mx-auto h-full">
-        <div className="pt-4 sm:p-6 lg:p-8">
-          <div className="pb-4 px-4 sm:px-0 sm:pb-6 lg:pb-8">
-            <Search searchRef={searchRef} setInput={setInput} />
-          </div>
-          {data === null ? (
-            <div>
-              <svg
-                className="h-5 w-5 text-gray-400"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
-              </svg>
-            </div>
-          ) : Array.isArray(data) ? (
-            <ImageContainer data={data} />
-          ) : data.results?.length > 0 ? (
-            <ImageContainer data={data.results} />
-          ) : (
-            <div className="flex flex-col flex-1 justify-center space-y-4 sm:space-y-6 lg:space-y-8">
-              <BlankCanvas />
-              <span className="text-center text-gray-600">
-                Sorry, we couldn't find any matches.
-              </span>
-            </div>
-          )}
+
+  const [ref, inView] = useInView()
+
+  useEffect(() => {
+    if (inView) {
+      fetchMore()
+    }
+  }, [inView])
+
+  if (status === "loading")
+    return <Loading input={input} searchRef={searchRef} setInput={setInput} />
+
+  if (status === "error") {
+    return <Error searchRef={searchRef} setInput={setInput} />
+  }
+  if (status === "success" && data.flat().length == 0)
+    return <NoData searchRef={searchRef} setInput={setInput} />
+
+  if (data)
+    return (
+      <>
+        <Head>
+          <title>
+            {input
+              ? `${input.charAt(0).toUpperCase()}${input
+                  .slice(1)
+                  .toLowerCase()} - `
+              : ""}
+            Image Gallery
+          </title>
+        </Head>
+        <div className=" px-4 sm:px-0 pb-4 sm:pb-6 lg:pb-8">
+          <Search searchRef={searchRef} setInput={setInput} />
         </div>
-      </div>
-      <div ref={ref} style={{ border: "1px solid red" }}></div>
-    </>
-  )
+        <ImageContainer data={data} />
+        <div ref={ref} className="mb-4" />
+      </>
+    )
 }
